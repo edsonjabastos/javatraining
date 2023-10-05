02a8c3a5060b45bd9bb198a93263227a63f0159f
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-10-04 21:03:28 -0300

    Create Order.java

---
+++ EX28/src/entities/Order.java
@@ -0,0 +1,95 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+	Date moment;
+	OrderStatus status;
+
+	List<OrderItem> order = new ArrayList<>();
+	Client client;
+
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public List<OrderItem> getOrder() {
+		return order;
+	}
+
+	public void setOrder(List<OrderItem> order) {
+		this.order = order;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public void addItem(OrderItem item) {
+		order.add(item);
+	}
+
+	public void removeItem(OrderItem item) {
+		order.remove(item);
+	}
+
+	public Double total() {
+		double total = 0.0;
+		for (OrderItem item : order) {
+			total += item.subTotal();
+		}
+		return total;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem item : order) {
+			sb.append(item + "\n");
+		}
+		sb.append("Total price: $");
+		sb.append(String.format("%.2f", total()));
+		return sb.toString();
+	}
+}
